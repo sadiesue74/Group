1c220bc7026e81a48a5d39c3457afabf145bfd85
Dillstan <75856962+Dillstan@users.noreply.github.com>
2024-09-23 09:33:29 -0400

    Added and removed some files
    Added the Clipboard Parsed function from work

--- MaterialList/MaterialList/ExtraMethods.cs
+++ MaterialList/MaterialList/ExtraMethods.cs
@@ -8,6 +8,39 @@ namespace MaterialList
 {
     public static class ExtraMethods
     {
+        #region GetClipboardData
+
+        private static String[] _LineSplit = new String[1] { Environment.NewLine };
+
+        public static string[] SuperSplit(this string InputString, int MinimumFields, char delimiter = '\t')
+        {
+            string[] results = InputString.Split(delimiter);
+            if (results.Length < MinimumFields)
+                Array.Resize(ref results, MinimumFields);
+
+            return results;
+        }
+
+        public static string ClipboardData
+        {
+            get
+            {
+                IDataObject iData = Clipboard.GetDataObject();
+                if (iData == null) return String.Empty;
+
+                if (iData.GetDataPresent(DataFormats.Text))
+                    return (string)iData.GetData(DataFormats.Text);
+                return String.Empty;
+            }
+        }
+
+        public static IEnumerable<string[]> ClipBoardDataParsed(int MinimumFields)
+        {
+            foreach (string line in ClipboardData.Split(_LineSplit, StringSplitOptions.RemoveEmptyEntries))
+                yield return line.SuperSplit(MinimumFields, '\t');
+        }
+        #endregion
+
 
     }
 }
